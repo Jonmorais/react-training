abdead1e84ebc6c2a485b7ddcca740a571c00f80
Jonmorais <jonathan_ms15@hotmail.com>
2021-08-28 16:12:52 -0300

    Created reducer

--- dark-mode-redux/script.js
+++ dark-mode-redux/script.js
@@ -1,1 +1,17 @@
+window.onload = () => {
+  const CHANGE_THEME = 'CHANGE_THEME'; // use to highlight the colors when using the CHANGE_THEME in the cases
+
+  function themeReducer(state = { theme: 'light' }, action) {
+    // Booting the state with the light theme
+    switch (action.type) {
+      case CHANGE_TO_DARK:
+        return { ...state, theme: 'dark' };
+      case CHANGE_TO_LIGHT:
+        return { ...state, theme: 'light' };
+      default:
+        return state;
+    }
+  }
+};
+
 const store = Redux.createStore(themeReducer);
