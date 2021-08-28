0b82822fd4af2f95b83f0bc509ec5bbfb6e38e6c
Jonmorais <jonathan_ms15@hotmail.com>
2021-08-28 16:46:56 -0300

    Created event listener with dispatch

--- dark-mode-redux/script.js
+++ dark-mode-redux/script.js
@@ -4,14 +4,29 @@ window.onload = () => {
   function themeReducer(state = { theme: 'light' }, action) {
     // Booting the state with the light theme
     switch (action.type) {
-      case CHANGE_TO_DARK:
-        return { ...state, theme: 'dark' };
-      case CHANGE_TO_LIGHT:
-        return { ...state, theme: 'light' };
+      // case CHANGE_TO_DARK:
+      //   return { ...state, theme: 'dark' };
+      // case CHANGE_TO_LIGHT:
+      //   return { ...state, theme: 'light' }; This is the first way we think, but...
+      case CHANGE_THEME:
+        return { ...state, theme: action.payload.theme } // Payload is a non-official, 
+        // community accepted naming convention for the property that holds the actual 
+        // data in a Redux action object.
       default:
         return state;
     }
   }
 };
 
-const store = Redux.createStore(themeReducer);
+const store = Redux.createStore(themeReducer); // The store receives a reducer as a parameter.
+
+const lightSwitch = document.querySelector('#light-switch');
+
+lightSwitch.addEventListener('click', () => {
+  const { theme } = store.getState(); // Recover the selected theme
+  if(theme === 'light') {
+    store.dispatch({ type: CHANGE_THEME, payload: { theme: 'dark' }});
+  } else {
+    store.dispatch({ type:CHANGE_THEME, payload: { theme: 'light' } })
+  }
+});
